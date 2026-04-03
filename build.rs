5fbd15d7ffe873faa6068ee58e62111d0265a73f
David <d>
2026-04-02 20:46:07 -0500

    inst spread filter

--- build.rs
+++ build.rs
@@ -1,4 +1,7 @@
 fn main() {
+    println!("cargo:rerun-if-changed=proto/");
+    println!("cargo:rerun-if-changed=build.rs");
+
     let mut config = prost_build::Config::new();
 
     // Compile the proto files we need
